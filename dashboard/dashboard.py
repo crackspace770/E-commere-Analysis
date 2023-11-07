ce3f01ca8926b8f6221cd5b8671cfba9e063ef9e
fajar <crackspace770>
2023-11-07 19:08:30 +0700

    change file

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
+import seaborn as sns
 from babel.numbers import format_currency
 
 sns.set(style='dark')
